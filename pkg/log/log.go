0da2b1f98144433b2bca4a62d28a34e5d407912f
DeathLimbo <1097125960@qq.com>
2025-08-12 16:43:40 +0800

    日志系统上传 zap 日志

---
+++ pkg/log/log.go
@@ -0,0 +1,85 @@
+package log
+
+import (
+	"go.uber.org/zap"
+	"go.uber.org/zap/zapcore"
+	"gopkg.in/natefinch/lumberjack.v2"
+	"os"
+	"time"
+)
+
+type BaseLogger struct {
+	*zap.Logger
+}
+
+var logger *BaseLogger
+
+func init() {
+	zapLevel := initZapLogLev("debug") //TODO: 暂时这样写，后续服务器搭建完成 通过配置获取
+
+	encoderCfg := initJsonCfg()
+	// 同时输出到文件和控制台
+	core := zapcore.NewTee(
+		zapcore.NewCore(zapcore.NewJSONEncoder(encoderCfg), initSyncer("log/debug.log"), zapLevel),
+		zapcore.NewCore(zapcore.NewConsoleEncoder(encoderCfg), zapcore.AddSync(os.Stdout), zapLevel),
+	)
+
+	zapLogger := zap.New(core, zap.AddCaller(), zap.AddCallerSkip(1))
+
+	logger = &BaseLogger{zapLogger}
+}
+
+func initZapLogLev(level string) zapcore.Level {
+	// 日志等级
+	var zapLevel zapcore.Level
+	switch level {
+	case "debug":
+		zapLevel = zapcore.DebugLevel
+	case "info":
+		zapLevel = zapcore.InfoLevel
+	case "warn":
+		zapLevel = zapcore.WarnLevel
+	case "error":
+		zapLevel = zapcore.ErrorLevel
+	default:
+		zapLevel = zapcore.InfoLevel
+	}
+	return zapLevel
+}
+
+func initSyncer(logPath string) zapcore.WriteSyncer {
+	// 日志文件滚动配置
+	writeSyncer := zapcore.AddSync(&lumberjack.Logger{
+		Filename:   logPath,
+		MaxSize:    100,  // MB
+		MaxBackups: 30,   // 最多保留30个备份
+		MaxAge:     7,    // 7天
+		Compress:   true, // 启用压缩
+	})
+	return writeSyncer
+}
+
+func initJsonCfg() zapcore.EncoderConfig {
+	// 编码器（JSON 格式）
+	encoderConfig := zapcore.EncoderConfig{
+		TimeKey:       "time",
+		LevelKey:      "level",
+		NameKey:       "logger",
+		CallerKey:     "caller",
+		MessageKey:    "msg",
+		StacktraceKey: "stacktrace",
+		LineEnding:    zapcore.DefaultLineEnding,
+		EncodeLevel:   zapcore.LowercaseLevelEncoder,
+		EncodeTime: func(t time.Time, enc zapcore.PrimitiveArrayEncoder) {
+			enc.AppendString(t.Format("2006-01-02 15:04:05.000000000"))
+		},
+		EncodeCaller: zapcore.ShortCallerEncoder,
+	}
+	return encoderConfig
+}
+
+// 快捷调用
+func Debug(msg string, fields ...zap.Field) { logger.Debug(msg, fields...) }
+func Info(msg string, fields ...zap.Field)  { logger.Info(msg, fields...) }
+func Warn(msg string, fields ...zap.Field)  { logger.Warn(msg, fields...) }
+func Error(msg string, fields ...zap.Field) { logger.Error(msg, fields...) }
